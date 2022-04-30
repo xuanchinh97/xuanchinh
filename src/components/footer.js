950f839bdf691767820e427856807384bb46d884
Xuan Chinh <87141231+xuanchinh97@users.noreply.github.com>
2022-04-30 20:17:23 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A litte {" "}
+        A little {" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
